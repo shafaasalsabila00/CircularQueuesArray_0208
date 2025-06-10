24ca1c6c5b1ab1f9ccab342118e86f69e2763ca8
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-06-10 11:04:35 +0700

    menambahkan iterasi

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -74,5 +74,18 @@ public:
         cout << "\nElements in the queue are...\n";
 
         //jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "  ";
+            }
+            cout << endl;
+        }
+        else {
+            //jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position;
+            }
+        }
     }
 };
