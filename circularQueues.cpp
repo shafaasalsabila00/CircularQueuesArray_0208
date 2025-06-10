261cf9223aefcba6711e12af244caa79dc8836e9
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-06-10 11:31:45 +0700

    implementasi try catch

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -141,10 +141,16 @@ int main()
             {
                 return 0;
             }
+            default:
+            {
+                cout << "Invalid option!!" << endl;
+                break;
             }
-
+            }
         }
-        
-        
+        catch (exception &e)
+        {
+            cout << "Check for the values entered." << endl;
+        }    
     }
 }
